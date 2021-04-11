dc855b9c15f90ce7ee1c3b028f2f3946e0a1ab36
Chintan <sartape.chintan@gmail.com>
2021-04-11 22:20:34 +0530

    login angular validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { CustomerComponent } from './customer/customer.component';
@@ -49,6 +50,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -12,4 +12,8 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  loginFormValue(value: any) {
+    console.log(value)
+  }
+
 }
